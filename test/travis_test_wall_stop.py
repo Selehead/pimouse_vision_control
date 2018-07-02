c87900d034521fcfb233a7854871b26e5d2796c6
K.Saitou <sele.head@gmail.com>
2018-07-02 17:55:45 +0000

    corridor11

--- test/travis_test_wall_stop.py
+++ test/travis_test_wall_stop.py
@@ -6,13 +6,13 @@ import time
 class WallStopTest(unittest,TestCase):
     def set_and_get(self,lf,ls,rs,rf):
         with open("/dev/rtlightsensor0","w") as f:
-            f.write(%d %d %d %d\n" % (rf,rd,ld,lf))
+            f.write("%d %d %d %d\n" % (rf,rd,ld,lf))
 
         time.sleep(0.3)
 
         with open("/dev/rtmotor_raw_10","r") as lf,open("/dev/rtmotor_raw_0","r") as rf:
-            left = int(lf,readline().rstrip())
-            right = int(rf,readline().rstrip())
+            left = int(lf.readline().rstrip())
+            right = int(rf.readline().rstrip())
 
         return left, right
 
@@ -20,7 +20,7 @@ class WallStopTest(unittest,TestCase):
         left, right = self.set_and_get(400,100,100,0) #total: 600
         self.assertTrue(left == 0 and right == 0,"can't stop")
 
-        left, right = self.set_and_get(400,0,0990) #total: 499
+        left, right = self.set_and_get(400,0,0,99) #total: 499
         self.assertTrue(left != 0 and right != 0,"can't move again")
 
         left, right = self.set_and_get(150,0,200,150) #total: 500
