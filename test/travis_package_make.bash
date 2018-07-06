4fce4b34ee758622a0f304af9169770c80750e70
K.Saitou <sele.head@gmail.com>
2018-07-06 10:40:44 +0000

    corrtidor17

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -5,7 +5,7 @@ rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor
 
 #cloneする
 cd ~/catkin_ws/src/
-git clone --depth=1 https://github.com/Selehead/pimouse_ros.git
+git clone --depth=1 https://github.com/citueda/pimouse_ros.git
         # =1は最新
 
 cd ~/catkin_ws/
